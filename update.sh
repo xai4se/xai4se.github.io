d7881ac64b8e53d3f02900b29dbe4039264f5f8b
Jirayus Jiarpakdee <jirayus.jiar@gmail.com>
2021-03-30 11:12:31 +1100

    update an update script

--- update.sh
+++ update.sh
@@ -1,4 +1,11 @@
 #!bin/bash
 
+# delete branch locally
+git branch -d gh-pages
+
+# delete branch remotely
+git push origin --delete gh-pages
+
+# re-build a jupyter book and push gh-pages
 jupyter-book build docs/
 ghp-import -n -p -c xai4se.github.io -f docs/_build/html
