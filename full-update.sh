eb08cba287693dc296d566c9e2ce7268b4751964
Jirayus Jiarpakdee <jirayus.jiar@gmail.com>
2021-04-30 16:28:00 +1000

    - Add new logo - Fix XAI4SE chapters - Update new illustrative examples of instance explanations

--- full-update.sh
+++ full-update.sh
@@ -1,11 +1,6 @@
 #!bin/bash
 
-# delete branch locally
-git branch -D gh-pages
-
-# delete branch remotely
-git push origin --delete gh-pages
-
+rm -rf docs/_build
 # re-build a jupyter book and push gh-pages
 jupyter-book build docs/
 ghp-import -n -p -c xai4se.github.io -f docs/_build/html
--- update.sh
+++ update.sh
@@ -3,4 +3,3 @@
 rm -rf docs/_build
 # re-build a jupyter book and push gh-pages
 jupyter-book build docs/
-ghp-import -n -p -c xai4se.github.io -f docs/_build/html
