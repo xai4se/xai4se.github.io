723a5641685181173775dc16f633e6a8e3760784
Michael Fu <michaelfu1998@gmail.com>
2021-11-12 19:01:50 +1100

    Update full-update.sh

--- full-update.sh
+++ full-update.sh
@@ -1,6 +1,3 @@
-#!bin/bash
-
 rm -rf docs/_build
-# re-build a jupyter book and push gh-pages
-jupyter-book build docs/
+jupyter-book build docs
 ghp-import -n -p -c xai4se.github.io -f docs/_build/html
